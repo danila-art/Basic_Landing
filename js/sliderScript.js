310a7d5a2e75e621803c9893de02a7830eee799d
danila-art <danila.sivov.96@mail.ru>
2023-04-29 18:48:59 +0400

    complete 90%

--- js/sliderScript.js
+++ js/sliderScript.js
@@ -6,7 +6,7 @@ translate = 0
 let mouseStartX = 0
 while (i < items.length) {
     items[i].addEventListener('mousedown', () => {
-            mouseStartX = event.clientX
+        mouseStartX = event.clientX
         document.addEventListener('mousemove', translateX)
     })
     window.addEventListener('mouseup', () => {
@@ -17,7 +17,7 @@ while (i < items.length) {
 }
 
 function translateX(event) {
-    setTimeout(()=>mouseStartX = event.clientX, 100)
+    setTimeout(() => mouseStartX = event.clientX, 100)
     if (mouseStartX > event.clientX) {
         if (translate == -50) {
             console.log('Нельзя');
@@ -35,7 +35,80 @@ function translateX(event) {
             console.log(translate)
         }
     }
-  
-}
 
+}
+let slide1 = document.getElementById('slide1')
+let labelSlide1 = document.getElementById('labelSlide1')
+slide1.addEventListener('click', () => {
+    translate = 0
+    itemsBlock.style.transition = '.5s'
+    itemsBlock.style.transform = 'translate(' + translate + '%)';
+    setTimeout(() => {
+        itemsBlock.style.removeProperty('transition')
+    }, 600)
+    labelSlide1.style.backgroundColor = "rgba(255, 68, 0, 0.9)"
+    labelSlide1.style.transform = 'scale(1.1)'
+    labelSlide2.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide2.style.transform = 'scale(1)'
+    labelSlide3.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide3.style.transform = 'scale(1)'
+    labelSlide4.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide4.style.transform = 'scale(1)'
+});
+
+let slide2 = document.getElementById('slide2')
+let labelSlide2 = document.getElementById('labelSlide2')
+slide2.addEventListener('click', () => {
+    translate = -18
+    itemsBlock.style.transition = '.5s'
+    itemsBlock.style.transform = 'translate(' + translate + '%)';
+    setTimeout(() => {
+        itemsBlock.style.removeProperty('transition')
+    }, 600)
+    labelSlide1.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide1.style.transform = 'scale(1)'
+    labelSlide2.style.backgroundColor = "rgba(255, 68, 0, 0.9)"
+    labelSlide2.style.transform = 'scale(1.1)'
+    labelSlide3.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide3.style.transform = 'scale(1)'
+    labelSlide4.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide4.style.transform = 'scale(1)'
+});
+let slide3 = document.getElementById('slide3')
+let labelSlide3 = document.getElementById('labelSlide3')
+slide3.addEventListener('click', () => {
+    translate = -33
+    itemsBlock.style.transition = '.5s'
+    itemsBlock.style.transform = 'translate(' + translate + '%)';
+    setTimeout(() => {
+        itemsBlock.style.removeProperty('transition')
+    }, 600)
+    labelSlide1.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide1.style.transform = 'scale(1)'
+    labelSlide2.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide2.style.transform = 'scale(1)'
+    labelSlide3.style.backgroundColor = "rgba(255, 68, 0, 0.9)"
+    labelSlide3.style.transform = 'scale(1.1)'
+    labelSlide4.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide4.style.transform = 'scale(1)'
+});
+
+let slide4 = document.getElementById('slide4')
+let labelSlide4 = document.getElementById('labelSlide4')
+slide4.addEventListener('click', () => {
+    translate = -50
+    itemsBlock.style.transition = '.5s'
+    itemsBlock.style.transform = 'translate(' + translate + '%)';
+    setTimeout(() => {
+        itemsBlock.style.removeProperty('transition')
+    }, 600)
+    labelSlide1.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide1.style.transform = 'scale(1)'
+    labelSlide2.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide2.style.transform = 'scale(1)'
+    labelSlide3.style.backgroundColor = "rgba(255, 68, 0, 0.4)"
+    labelSlide3.style.transform = 'scale(1)'
+    labelSlide4.style.backgroundColor = "rgba(255, 68, 0, 0.9)"
+    labelSlide4.style.transform = 'scale(1.1)'
+});
 

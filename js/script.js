f01e85051ae6dd9cc64db30433ebce662a547a20
danila-art <danila.sivov.96@mail.ru>
2023-05-04 18:23:57 +0400

    complete 99%

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ window.addEventListener('scroll', () => {
         document.getElementById('main__header').style.backgroundColor = 'white'
         document.getElementById('headerLogo').src = "img//logo-2.svg"
         let arrButton = document.getElementsByClassName('navButtonA')
-        let i = 0 
-        while (i < arrButton.length){
+        let i = 0
+        while (i < arrButton.length) {
             arrButton[i].style.color = 'black'
             i++
         }
@@ -13,11 +13,21 @@ window.addEventListener('scroll', () => {
         document.getElementById('main__header').style.backgroundColor = 'rgba(0, 0, 0, 0.0)'
         document.getElementById('headerLogo').src = "img//logo.svg"
         let arrButton = document.getElementsByClassName('navButtonA')
-        let i = 0 
-        while (i < arrButton.length){
+        let i = 0
+        while (i < arrButton.length) {
             arrButton[i].style.color = 'white'
             i++
         }
     }
 })
 
+
+let moduleBlock = document.getElementById('moduleBlock')
+let moduleBlockClose = document.getElementById('moduleBlockClose')
+let buttonOpenModuleBlock = document.getElementById('buttonOpenModuleBlock')
+buttonOpenModuleBlock.addEventListener('click', () => {
+    moduleBlock.style.display = "block"
+    moduleBlockClose.addEventListener('click', () => {
+        moduleBlock.style.display = 'none'
+    })
+})
